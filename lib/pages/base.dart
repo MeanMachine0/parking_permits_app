4d6625ea49eaf252f0934156f41a48a780ea34e4
MeanMachineMarc <marcus9981@icloud.com>
2023-05-26 22:32:02 +0100

    early navbar

---
+++ lib/pages/base.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import 'home.dart';
+import 'login.dart';
+
+class Base extends StatefulWidget {
+  const Base({Key? key}) : super(key: key);
+
+  @override
+  _BaseState createState() => _BaseState();
+}
+
+class _BaseState extends State<Base> {
+  int _selectedIndex = 0;
+  static const List<Widget> pages = <Widget>[
+    Home(),
+    Login(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages.elementAt(_selectedIndex),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colours.deepDarkGray,
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Login',
+          ),
+        ],
+      ),
+    );
+  }
+}
