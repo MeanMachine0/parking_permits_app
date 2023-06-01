22e64eb0086ec76ca3a8701414a7f5f9904bd74a
MeanMachineMarc <marcus9981@icloud.com>
2023-06-01 17:33:53 +0100

    first version of settings page (detailed view option)

---
+++ lib/pages/settings.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool isLoading = false;
+  bool? hasSettings;
+  bool detailedView = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    if (mounted) {
+      setState(() {
+        isLoading = true;
+      });
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    hasSettings = prefs.getBool('hasSettings');
+    if (hasSettings == null) {
+      await prefs.setBool('detailedView', false);
+    } else {
+      detailedView = prefs.getBool('detailedView')!;
+    }
+    if (mounted) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Column(
+                children: [
+                  CheckboxListTile(
+                    title: const Text('Detailed View'),
+                    value: detailedView,
+                    onChanged: (newValue) async {
+                      SharedPreferences prefs =
+                          await SharedPreferences.getInstance();
+                      prefs.setBool('detailedView', newValue!);
+                      setState(() {
+                        detailedView = !detailedView;
+                      });
+                    },
+                  )
+                ],
+              ));
+  }
+}
