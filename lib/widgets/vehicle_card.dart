bf6a4587f716f2f0c2c8ddf6edd01dd20ef89745
MeanMachineMarc <marcus9981@icloud.com>
2023-06-04 15:51:41 +0100

    Improved design, but not completed

--- lib/widgets/vehicle_card.dart
+++ lib/widgets/vehicle_card.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../constants.dart';
 import '../models/permit_model.dart';
 
 class VehicleCard extends StatelessWidget {
@@ -15,34 +18,57 @@ class VehicleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+          padding: const EdgeInsets.all(10),
           child: Column(
             children: [
+              // Row(
+              //   children: [
+              //     Expanded(
+              //       child: TextField(
+              //         decoration: const InputDecoration(labelText: 'Note'),
+              //         controller: TextEditingController(text: _vehicle.note),
+              //         onSubmitted: (newNote) async {
+              //           SharedPreferences prefs =
+              //               await SharedPreferences.getInstance();
+              //           await prefs.setString(_vehicle.id.toString(), newNote);
+              //         },
+              //       ),
+              //     )
+              //   ],
+              // ),
               Row(
                 children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: const InputDecoration(labelText: 'Note'),
-                      controller: TextEditingController(text: _vehicle.note),
-                      onSubmitted: (newNote) async {
-                        SharedPreferences prefs =
-                            await SharedPreferences.getInstance();
-                        await prefs.setString(_vehicle.id.toString(), newNote);
-                      },
-                    ),
-                  )
+                  Text(_vehicle.vrm,
+                      style: const TextStyle(
+                        fontFamily: 'Courier New',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      )),
+                  if (_vehicle.note != '') Text(' - ${_vehicle.note}'),
+                  if (_vehicle.isActive) const SizedBox(width: 4),
+                  Expanded(child: Container()),
+                  if (_vehicle.isActive)
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 3),
+                      color: Colors.green,
+                      alignment: Alignment.centerRight,
+                      child: const Text(
+                        'PERMIT ASSIGNED',
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                    )
                 ],
               ),
-              Row(
-                children: [Text(_vehicle.vrm)],
-              ),
-              Row(
-                children: [
-                  Text(
-                    _vehicle.id.toString(),
-                  ),
-                ],
-              )
+              // Row(
+              //   children: [
+              //     Text(
+              //       _vehicle.id.toString(),
+              //     ),
+              //   ],
+              // ),
             ],
           )),
     );
