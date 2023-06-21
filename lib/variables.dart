3e6c36a8b2383a7013cf83fe226daf979e9fc737
MeanMachineMarc <marcus9981@icloud.com>
2023-06-21 17:29:09 +0100

    Updated

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,5 +1,25 @@
+import 'instances.dart';
+
 class Variables {
-  static bool isLoading = false;
-  static bool isLoading1 = false;
-  static bool notSilentlySigningIn = true;
+  static bool isLoading = false,
+      isLoading1 = false,
+      notSilentlySigningIn = true;
+  static late bool bioOrLocalAuthIsSupported,
+      useBioOrLocalAuth,
+      useDetailedView,
+      isReorderable;
+  static late String dateFormat;
+  static late String? parkingEmail;
+  static late List<String> tokens;
+
+  static Future<void> initialise() async {
+    bioOrLocalAuthIsSupported = await Instances.localAuth.isDeviceSupported();
+    useBioOrLocalAuth = Instances.prefs.getBool('useBioOrLocalAuth') ??
+        bioOrLocalAuthIsSupported;
+    useDetailedView = Instances.prefs.getBool('useDetailedView') ?? false;
+    isReorderable = Instances.prefs.getBool('isReorderable') ?? false;
+    dateFormat = Instances.prefs.getString('dateFormat') ?? 'dd/MM/yyyy';
+    parkingEmail = Instances.prefs.getString('parkingEmail');
+    tokens = Instances.prefs.getStringList('tokens') ?? [];
+  }
 }
